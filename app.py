2ace5b0b76c0d5e258be5e94b4548051c33722ee
Robbie Thomas <robbiekt@gmail.com>
2023-04-04 13:53:10 -0700

    add item data and render it on page

--- app.py
+++ app.py
@@ -2,9 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+ITEMS = [
+  {
+    'id': 1,
+    'title': 'Paddle',
+    'price': 29.99,
+    'description': 'beautiful rowing paddle'
+  },
+   {
+    'id': 2,
+    'title': 'Canoe',
+    'price': 2999.99,
+    'description': 'Brand new wooden conoe'
+  },
+   {
+    'id': 3,
+    'title': 'Life Jacket',
+    'price': 14.99,
+    'description': 'slightly used life jacket, medium sized'
+  },
+     {
+    'id': 4,
+    'title': 'Kayak',
+    'price': 1899.99,
+    'description': 'white water kayak, slightly used'
+  },
+]
+
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', items=ITEMS)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
